83aac6dd3202f77e77ad1e33c691fe5116c19189
Woody <2997336+MWGMorningwood@users.noreply.github.com>
2024-09-09 20:34:07 -0400

    Refactor dynamic integration script
    This commit refactors the "Dynamic Integration Type - Agent Smith.ps1" script
    
    In the script, the [Password] attribute for the $RegistrationSecret parameter is changed to [String]. This change allows the RegistrationSecret to be passed as a plain string instead of a secure password object. This will be addressed later in a more secure way.
    
    Additionally, the script execution parameters have been tweaked to pass the exact ExternalID of the agent. This was done because I believe oddities of the inegration due to either platform should be solved in the platform with the oddity and not piled onto the functionality of one end.

--- ImmyBot Integration/Agent Smith Dynamic Integration Script.ps1
+++ ImmyBot Integration/Agent Smith Dynamic Integration Script.ps1
@@ -4,16 +4,30 @@
 
 $Integration = New-DynamicIntegration -Init {
     param(
-        [Parameter(Mandatory)]
+        
+        [Parameter(Mandatory = $false, HelpMessage='URL of the repo containing Agent Smith releases')]
+        [String]$GitRepoURL = 'https://github.com/RewstApp/rewst_remote_agent',
+
+        [Parameter(Mandatory = $true, HelpMessage='URL of the Agent Registration Workflow trigger')]
+        [String]$RegistrationWebhook,
+
+        [Parameter(Mandatory = $true, HelpMessage='Org Secret set in Rewst')]
+        [String]$RegistrationSecret,
+
+        [Parameter(Mandatory = $true, HelpMessage='URL of `Get_Orgs` trigger of the Foghorn Workflow')]
         [String]$OrgWebhook,
-        [Parameter(Mandatory)]
+
+        [Parameter(Mandatory = $true, HelpMessage='URL of `Health_Check` trigger of the Foghorn Workflow')]
         [String]$HealthWebhook,
-        [Parameter(Mandatory)]
+
+        [Parameter(Mandatory = $true, HelpMessage='URL of `Get_Agents` trigger of the Foghorn Workflow')]
         [String]$AgentsWebhook,
-        [Parameter(Mandatory)]
+
+        [Parameter(Mandatory = $true, HelpMessage='URL of `Run_Command` trigger of the Foghorn Workflow')]
         [String]$CommandWebhook,
-        [Parameter(Mandatory)]
-        [Password(StripValue = $true)]$ApiKey
+
+        [Parameter(Mandatory = $true, HelpMessage='Org Secret set in Rewst')]
+        [Password(StripValue)]$ApiKey
     )
     Write-Host "Initializing Agent Smith"
     $IntegrationContext.SmithApiKey = $ApiKey
@@ -21,13 +35,16 @@ $Integration = New-DynamicIntegration -Init {
     $IntegrationContext.SmithOrgWebhook = $OrgWebhook
     $IntegrationContext.SmithAgentWebhook = $AgentsWebhook
     $IntegrationContext.SmithCommandWebhook = $CommandWebhook
+    $IntegrationContext.SmithRegistrationWebhook = $RegistrationWebhook
+    $IntegrationContext.SmithRegistrationSecret = $RegistrationSecret
+    $IntegrationContext.SmithGitRepoURL = $GitRepoURL
+
 
     [OpResult]::Ok() 
 } -HealthCheck { 
     Import-Module AgentSmithAPI
     Get-SmithAPIHealth
 }
-
 
 $Integration | Add-DynamicIntegrationCapability -Interface ISupportsListingClients -GetClients {
     [ScriptTimeout(TimeoutSeconds = 300)]
@@ -37,9 +54,14 @@ $Integration | Add-DynamicIntegrationCapability -Interface ISupportsListingClien
     try{
         Import-Module AgentSmithAPI
         $Orgs = Get-SmithOrgID
+        $managingOrgAdded = $false
         $Orgs | ForEach-Object {
-            if ($_.OrgId -and $_.OrgName) {
-                New-IntegrationClient -ClientId $_.OrgId -ClientName $_.OrgName
+            if (!$managingOrgAdded -and $_.managingOrg.id -and $_.managingOrg.name) {
+                New-IntegrationClient -ClientId $_.managingOrg.id -ClientName $_.managingOrg.name
+                $managingOrgAdded = $true
+            }
+            if ($_.id -and $_.name) {
+                New-IntegrationClient -ClientId $_.id -ClientName $_.name
             } else {
                 Write-Error "Not enough data for $_"
             }
@@ -61,7 +83,7 @@ $Integration | Add-DynamicIntegrationCapability -Interface ISupportsListingAgent
 
     $currentTime = Get-Date
 
-    Get-SmithAgent | % {
+    Get-SmithAgent | ForEach-Object {
         $timestampDateTime = [DateTime]::Parse($_.Timestamp)
         $timeDifference = $currentTime - $timestampDateTime
         $online = ($timeDifference.TotalMinutes -le 5)
@@ -76,7 +98,6 @@ $Integration | Add-DynamicIntegrationCapability -Interface ISupportsListingAgent
     }
 }
 
-
 $Integration | Add-DynamicIntegrationCapability -Interface ISupportsInventoryIdentification -GetInventoryScript {
     [CmdletBinding()]
     [OutputType([scriptblock])]
@@ -85,22 +106,57 @@ $Integration | Add-DynamicIntegrationCapability -Interface ISupportsInventoryIde
     )
     Invoke-ImmyCommand {
         # implement a script block that should retrieve the agent identifier for this integration.
-        Get-Content "C:\ProgramData\RewstRemoteAgent\*\config.json" | ConvertFrom-Json | % {$_.device_id}
+        Get-Content "C:\ProgramData\RewstRemoteAgent\*\config.json" | ConvertFrom-Json | ForEach-Object {$_.device_id}
     }
 }
 
+$Integration | Add-DynamicIntegrationCapability -Interface ISupportsDynamicVersions -GetDynamicVersions {
+    [CmdletBinding()]
+    [OutputType([Immybot.Backend.Domain.Models.DynamicVersion[]])]
+    param(
+        [Parameter(Mandatory=$True)]
+        [System.String]$externalClientId
+    )
+
+    $version = Get-DynamicVersionsFromGitHubUrl `
+    -GitHubReleasesUrl "$($IntegrationContext.SmithGitRepoURL)/releases" `
+    -VersionsPattern ('(?<Uri>'+$IntegrationContext.SmithGitRepoURL+'/releases/download/v(?<Version>[\d\.]+)/(?<FileName>rewst_agent_config.win.exe))')
+    
+    return $version.Versions
+}
+
+$Integration |  Add-DynamicIntegrationCapability -Interface ISupportsTenantInstallToken -GetTenantInstallToken {
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$clientId
+    )
+    $properties = @{
+        secret = $IntegrationContext.SmithRegistrationSecret
+        webhook = $IntegrationContext.SmithRegistrationWebhook
+    }
+    $object = New-Object -TypeName PSObject -Property $properties
+    return ($object | ConvertTo-Json)
+}
+
 $Integration | Add-DynamicIntegrationCapability -Interface IRunScriptProvider -RunScript {
+    [CmdletBinding()]
+    [OutputType([string])]
     param(
         [Parameter(Mandatory)]
         [IProviderAgentDetails]$agent,
         [Parameter(Mandatory)]
         [string]$scriptCode,
         [Parameter(Mandatory)]
-        [int]$timeout
+        [ScriptLanguage]$scriptLanguage,
+        [Parameter(Mandatory)]
+        [int]$timeout,
+        [Parameter(Mandatory)]
+        [string]$scriptPath
     )
     Import-Module AgentSmithAPI
-    Invoke-SmithCommand
+    Invoke-SmithCommand -agent $agent.ExternalAgentId -scriptCode $scriptCode
 } -get_DefaultTimeout { 600 }
-
 
 $Integration
